09f075740a23e5031fbf358b6904b0dfff57a460
MarMarcz <s24566@pjwstk.edu.pl>
2021-10-18 17:31:43 +0200

    some correction

--- src/s03-beer.cpp
+++ src/s03-beer.cpp
@@ -9,8 +9,8 @@ std::cout << b << " bottles of beer on the wall, " << b << " bottles of beer.\n"
 
 for(int i = b - 1; i >= 1; i--)
 {
-std::cout << "Take one down, pass it around, " << i << " bottles of beer on the wall...\n";
-std::cout << i << " bottles of beer on the wall, " << i << " bottles of beer.\n";
+	std::cout << "Take one down, pass it around, " << i << " bottles of beer on the wall...\n";
+	std::cout << i << " bottles of beer on the wall, " << i << " bottles of beer.\n";
 }
 
 std::cout << "No more bottles of beer on the wall, no more bottles of beer.\n";
